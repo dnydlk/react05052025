5dc4b615768c4224fae91e9cba64d4c828b43267
DaniyalDolkun <dnydlk@proton.me>
2025-05-11 22:52:50 -0400

    feat(functions): complete sum function

--- vanilla_homework/day5_js_basic2/functions.js
+++ vanilla_homework/day5_js_basic2/functions.js
@@ -1,9 +1,12 @@
-export function sum() {
+export function sum(a = 0, b = 0) {
   // write a function that takes two numbers as arguments and returns their sum
   // argument default values are 0
   // if wrong data type is passed, throw an error
   // Define your function here
-  return () => {};
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Wrong data type")
+  }
+  return a + b
 }
 
 export function sumOfAll() {
