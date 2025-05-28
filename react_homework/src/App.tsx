33d3b1538fc94caf9e8166df63649c15d0a94192
DaniyalDolkun <dnydlk@proton.me>
2025-05-28 12:50:22 -0400

    chore(day11): :wastebasket: code clean up

--- react_homework/src/App.tsx
+++ react_homework/src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css"
-import Homework from "./day11_describing_the_ui/homework"
 import Day11Notes from "./day11_describing_the_ui/notes"
 
 function App() {
   return (
     <div id="app">
       <Day11Notes />
-
-      <Homework />
     </div>
   )
 }
