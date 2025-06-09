5eb5e286ae4ef8ee0a7cbb6a0ca17ed8557b5145
DaniyalDolkun <dnydlk@proton.me>
2025-06-09 17:37:57 -0400

    feat(day21): :sparkles: add day 21 live lecture template

---
+++ react-js-redux/src/D21/LiveLecture/index.tsx
@@ -0,0 +1,7 @@
+export default function Day21LiveLecture() {
+  return (
+    <div className="d18-live-lecture">
+      <h3>Day 21 Live Lectures</h3>
+    </div>
+  )
+}
---
+++ react-js-redux/src/D21/index.jsx
@@ -0,0 +1,9 @@
+import Day21LiveLecture from "./LiveLecture"
+
+export default function D21() {
+  return (
+    <div className="day21-">
+      <Day21LiveLecture />
+    </div>
+  )
+}
