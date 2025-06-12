f8795453abfb7fcbfde8c105ab202823426ab6ed
DaniyalDolkun <dnydlk@proton.me>
2025-06-12 15:51:24 -0400

    feat(day17): :sparkles: completed DebouncedCallback function

--- react_homework/src/day17_performance/DebouncedCallback.tsx
+++ react_homework/src/day17_performance/DebouncedCallback.tsx
@@ -1,38 +1,42 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react"
+import { aF } from "vitest/dist/reporters-yx5ZTtEV.js"
 
 function debounce<F extends (...args: any[]) => any>(func: F, wait: number): F {
-  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  let timeoutId: ReturnType<typeof setTimeout> | null = null
 
   // This is a function that has the same parameters as `func`.
   // It uses a rest parameter syntax `...args` to capture all the passed arguments.
   const debouncedFunction = (...args: Parameters<F>) => {
     // Clear the current timeout, if there is one
     if (timeoutId !== null) {
-      clearTimeout(timeoutId);
+      clearTimeout(timeoutId)
     }
 
     // Set a new timeout
     timeoutId = setTimeout(() => {
-      func(...args);
-    }, wait);
-  };
+      func(...args)
+    }, wait)
+  }
 
   // Return the debounced function cast to the type F
-  return debouncedFunction as F;
+  return debouncedFunction as F
 }
 
 function apiCall(searchQuery: string) {
-  console.log("api called...");
+  console.log("api called with: ", searchQuery)
 }
 
 export default function DebouncedCallback() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const debouncedApiCall = useMemo(() => debounce(apiCall, 1000), [])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
+    setSearchTerm(e.target.value)
     // instead of using useDebounce hook, use debounce higher order function
-    apiCall(e.target.value);
-  };
+    //// apiCall(e.target.value)
+    debouncedApiCall(e.target.value)
+  }
 
   return (
     <div>
@@ -43,5 +47,5 @@ export default function DebouncedCallback() {
         onChange={handleChange}
       />
     </div>
-  );
+  )
 }
