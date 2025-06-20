4cd3c2d07b90bfaee7189dd7ee5ef54b43050f72
DaniyalDolkun <dnydlk@proton.me>
2025-06-20 01:04:06 -0400

    feat(day22): :sparkles: add post user & login routes with credential checks and jwt siging and verifying

--- node-practice/todoApp/server.js
+++ node-practice/todoApp/server.js
@@ -1,6 +1,15 @@
 import express from "express"
+import { comparePassword, hashPassword } from "./utils/bcrypt.js"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import { authenticateToken } from "./utils/middlewares/jwt.js"
+
 const app = express()
 const port = 3000
+
+app.use(express.json())
+
+dotenv.config()
 
 const posts = [
   {
@@ -13,8 +22,64 @@ const posts = [
   },
 ]
 
-app.get("/posts", (req, res) => {
-  res.json(posts)
+const users = []
+
+app.get("/posts", authenticateToken, (req, res) => {
+  console.log("📌 ~ app.get ~ req.user:", req.user)
+  // return the posts created by current user only
+  res.json(posts.filter((p) => p.username === req.user.name))
+})
+
+app.get("/users", (req, res) => {
+  // https://www.youtube.com/watch?v=Ud5xKCYQTjM
+  res.json(users)
+})
+
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassword = await hashPassword(req.body.password)
+    //#1 Create a new user
+    //#2 Hash the password
+    //#3 Save to Database(for now its user variable)
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+    }
+    users.push(user)
+    res.sendStatus(201)
+  } catch (error) {
+    console.error("Failed to create new user:", error)
+    res.sendStatus(500)
+  }
+})
+
+//- bcrypt only
+app.post("/login", async (req, res) => {
+  //#1 Authenticae User
+  // Find the usuer from db
+  const user = users.find((u) => u.name === req.body.name)
+  console.log("📌 ~ app.post ~ user:", user)
+  if (!user) return res.status(404).json({ error: "User not found" })
+
+  try {
+    // Compare password
+    if (await comparePassword(req.body.password, user.password)) {
+      //#2 JWT Authentication
+      const payload = { name: user.name }
+      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET)
+      console.log("📌 ~ app.post ~ payload:", payload)
+      res.status(200).json({ accessToken })
+    } else {
+      res.status(401).json({ error: "Wrong password" })
+    }
+  } catch (error) {
+    console.error("Failed:", error)
+    res.sendStatus(500)
+  }
+})
+
+app.get("/login", (req, res) => {
+  // Authenticate the user
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ node-practice/todoApp/utils/bcrypt.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt"
+
+const saltRounds = 10
+
+export const hashPassword = async (password) => {
+  try {
+    const hash = await bcrypt.hash(password, saltRounds)
+    // console.log(`Hashed password: ${hash}`)
+    return hash
+  } catch (error) {
+    console.error("Failed to hash password:", error)
+  }
+}
+
+export const comparePassword = async (password, hashedPassword) => {
+  try {
+    const result = await bcrypt.compare(password, hashedPassword)
+    console.log("📌 ~ comparePassword ~ result:", result)
+    return result
+  } catch (error) {
+    console.error("Failed to compare password:", error)
+  }
+}
+
+const example = async () => {
+  const myPlaintextPassword = "1234"
+  const someOtherPlaintextPassword = "qwer"
+  const myHashedPassword = await hashPassword(myPlaintextPassword)
+
+  await comparePassword(myPlaintextPassword, myHashedPassword)
+  await comparePassword(someOtherPlaintextPassword, myHashedPassword)
+}
